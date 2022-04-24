b62342a10dbc26f5d8180a9a7a957626333d543a
Stu Bolderson <stu.bolderson@aol.com>
2022-04-24 20:32:05 +0200

    click route from menu item to category page works

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+// npm
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+// files
+import { readCollection } from "../firebase/firestore";
 
 export default function Menu() {
-  return <div>Menu</div>;
+  const [dishes, setDishes] = useState([]);
+  const [status, setStatus] = useState(0);
+
+  // method
+  useEffect(() => {
+    async function loadData() {
+      const itemsData = await readCollection("Menu/Dishes/content");
+      console.log(itemsData);
+      setDishes(itemsData);
+      setStatus(1);
+    }
+    loadData();
+  }, []);
+
+  // safeguard
+  if (status === 0) return <p>Loading ..</p>;
+  if (status === 2) return <p>Error ..</p>;
+
+  const Categories = dishes.map((item) => (
+    <div key={item.id}>
+      <p>{item.title}</p>
+      <Link to={`/menu/${item.id}`}>See more</Link>
+    </div>
+  ));
+
+  return (
+    <section className="menu" id="menu">
+      <h2>Menu</h2>
+      <div>{Categories}</div>
+    </section>
+  );
 }
