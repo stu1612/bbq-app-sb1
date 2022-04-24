a70369dff72584631cd5c27cbda35ede2d7bc79f
Stu Bolderson <stu.bolderson@aol.com>
2022-04-24 19:14:23 +0200

    start up

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUgCRHZmrWNXj5pvimKVEBCFA60KKub_Q",
+  authDomain: "bbq-app-sb1.firebaseapp.com",
+  projectId: "bbq-app-sb1",
+  storageBucket: "bbq-app-sb1.appspot.com",
+  messagingSenderId: "606460872244",
+  appId: "1:606460872244:web:c75e4d82c8c6606a436c61",
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
