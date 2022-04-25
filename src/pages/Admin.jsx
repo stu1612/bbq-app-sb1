9b379b163242b9af4428b10d794362456083f705
Stu Bolderson <stu.bolderson@aol.com>
2022-04-25 08:33:31 +0200

    category products loaded

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+// npm
+import { useState, useEffect } from "react";
+// files
+import { readCollection } from "../firebase/firestore";
+// components
+import Loader from "../components/Loader";
+import ErrorMessage from "../components/ErrorMessage";
+import AdminCategoryItem from "../components/AdminCategoryItem";
 
 export default function Admin() {
-  return <div>Admin</div>;
+  const [dishes, setDishes] = useState([]);
+  const [status, setStatus] = useState(0);
+
+  // properties
+  const path = "Menu/Dishes/content";
+
+  // method
+  useEffect(() => {
+    async function loadData(path) {
+      const itemsData = await readCollection(path);
+      setDishes(itemsData);
+      setStatus(1);
+    }
+    loadData(path);
+  }, []);
+
+  // components
+  const Categories =
+    dishes &&
+    dishes.map((item) => <AdminCategoryItem key={item.id} item={item} />);
+
+  // safeguard
+  if (status === 0) return <Loader />;
+  if (status === 2) return <p>Error ..</p>;
+  if (dishes === undefined) return <ErrorMessage />;
+
+  return (
+    <div>
+      <h3>Categories</h3>
+      {Categories}
+    </div>
+  );
 }
